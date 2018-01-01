f29ff4346a96887bc5288b2a0c3b86b3c1387307
Daniel Quernheim <quernd@users.noreply.github.com>
2018-01-01 18:48:11 +0000

    scratch buffers, maps and lenses

--- src/Main.ml
+++ src/Main.ml
@@ -2,28 +2,78 @@ open Tea
 open Tea.Html
 open Tea.App
 
+open Lens.Infix
+
+module Int = struct
+  type t = int
+  let compare = compare
+end
+
+module String = struct
+  type t = string
+  let compare = compare
+end
+
+module IntMap = Map.Make(Int)
+module StringMap = Map.Make(String)
+
+module MapLens(Key : Map.OrderedType) = struct
+  let for_key key =
+    let module M = Map.Make(Key) in
+    let open Lens in
+    { get = M.find key
+    ; set = M.add key
+    }
+end
+
+module IntMapLens = MapLens(Int)
+module StringMapLens = MapLens(String)
+
 type 'a transfer =
+  | Idle
   | Loading
   | Failed
   | Received of 'a
 
-type route =
-  | Game
-  | Tournament
-  | Pgn of string
-
 type model =
-  { game : Game.model
+  { game : game_lens
   ; board : Board.model
+  ; scratch_game : Game.model
+  ; local_games : Game.model IntMap.t
   ; tournament : (string * string list) list transfer
-  ; pgn : (string * string transfer) list (* association list *)
+  ; lichess_games : (int * string transfer) StringMap.t
   ; route : route
   }
+and game_lens = (model, Game.model) Lens.t
+and route =
+  | Local of int
+  | Lichess of string
+  | Scratch
+  | Tournament
+
+
+
+let local_lens = let open Lens in
+  { get = (fun x -> x.local_games)
+  ; set = (fun v x -> { x with local_games = v })
+  }
+let lichess_lens = let open Lens in
+  { get = (fun x -> x.lichess_games)
+  ; set = (fun v x -> { x with lichess_games = v })
+  }
+let scratch_lens = let open Lens in
+  { get = (fun x -> x.scratch_game)
+  ; set = (fun v x -> { x with scratch_game = v })
+  }
+
+
 
 type msg =
   | Board_msg of Board.msg
   | Game_msg of Game.msg
   | Key_pressed of Keyboard.key_event
+  | Save_scratch
+  | Load_tournament
   | Tournament_data of (string, string Http.error) Result.t
   | Location_change of Web.Location.location  
   | Pgn_requested of string
@@ -38,34 +88,52 @@ let route_of_location location =
   let open Web.Location in
   let route = Chess.split_on_char '/' location.hash in
   match route with
-  | ["#"; "game"] -> Game, Cmd.none
-  | ["#"; "tournament"] -> Tournament, Cmd.none
-  | ["#"; "pgn"; id] ->  Pgn id, Cmd.msg (Pgn_requested id)
-  | _ -> Game, Navigation.modifyUrl "#/game"  (* default route *)
+  | ["#"; ""] -> Scratch
+  | ["#"; "tournament"] -> Tournament
+  | ["#"; "local"; id] -> Local (int_of_string id)
+  | ["#"; "lichess"; id] -> Lichess id
+  | _ -> Scratch
+
+let location_of_route = function
+  | Local id -> Printf.sprintf "#/local/%d" id
+  | Lichess id -> Printf.sprintf "#/lichess/%s" id
+  | Scratch -> "#/"
+  | Tournament -> "#/tournament"
+
+let update_route model route =
+  if model.route = route then model, Cmd.none
+  else
+    match route with
+    | Local id when IntMap.mem id model.local_games ->
+      let game = local_lens |-- IntMapLens.for_key id in
+      {model with route; game}, Cmd.none
+    | _ -> {model with route = Scratch; game = scratch_lens}, Cmd.none
+
+
+let init_model =
+  { scratch_game = Game.init ()
+  ; game = scratch_lens
+  ; board = Board.init ()
+  ; tournament = Idle
+  ; local_games = IntMap.empty
+  ; lichess_games = StringMap.empty
+  ; route = Scratch
+  }
 
 let init () location =
-  let route, cmd = route_of_location location in
-  let url = "https://lichess.org/api/tournament/GToVqkC9" in
-  let init_cmd =
-    Http.getString url |> Http.send tournament_data in
-  { game = Game.init ()
-  ; board = Board.init ()
-  ; tournament = Loading
-  ; route
-  ; pgn = []
-  }, Cmd.batch [init_cmd; cmd]
+  route_of_location location |> update_route init_model
 
 
 let update model = function
   | Board_msg (Move move) ->
-    let game, cmd = Game.update model.game (Game.Make_move move) in
-    {model with game}, Cmd.map game_msg cmd
+    let game, cmd = Game.update (model |. model.game) (Game.Make_move move) in
+    model |> model.game ^= game, Cmd.map game_msg cmd
   | Board_msg msg ->
     let board, cmd = Board.update model.board msg in
     {model with board}, Cmd.map board_msg cmd
   | Game_msg msg ->
-    let game, cmd = Game.update model.game msg in
-    {model with game}, Cmd.map game_msg cmd
+    let game, cmd = Game.update (model |. model.game) msg in
+    model |> model.game ^= game, Cmd.map game_msg cmd
   | Key_pressed key_event ->
     model,
     begin match key_event.ctrl, key_event.key_code with
@@ -77,6 +145,18 @@ let update model = function
       | true, 84 (* Ctrl-t *) -> Cmd.msg (Game_msg Back_button)
       | _ -> Cmd.none
     end
+  | Save_scratch ->
+    let key = try (IntMap.max_binding model.local_games |> fst) + 1
+      with Not_found -> 0 in
+    update_route
+      { model with
+        local_games = IntMap.add key (model |. model.game) model.local_games
+      ; scratch_game = Game.init ()
+      } (Local key)
+  | Load_tournament ->
+    let url = "https://lichess.org/api/tournament/GToVqkC9" in
+    {model with route = Tournament; tournament = Loading},
+    Http.getString url |> Http.send tournament_data
   | Tournament_data (Result.Error e) -> Js.log e;
     {model with tournament = Failed}, Cmd.none
   | Tournament_data (Result.Ok data) -> 
@@ -93,42 +173,8 @@ let update model = function
        | Error _ -> Failed
     }, Cmd.none
   | Location_change location ->
-    let route, cmd = route_of_location location in
-    {model with route}, cmd
-  | Pgn_requested id ->
-    begin
-      try
-        match List.assoc id model.pgn with
-        | Received pgn -> model, Cmd.msg (Validate_pgn pgn)
-        | _ -> model, Cmd.none
-      with Not_found -> 
-        let url =
-          Printf.sprintf "%shttps://lichess.org/game/export/%s.pgn" proxy id in
-        let cmd = Http.getString url |> Http.send (pgn_data id) in
-        { model with
-          route = Pgn id
-        ; pgn = (id, Loading)::model.pgn
-        }, cmd
-    end
-  | Pgn_data (id, Result.Error _e) ->
-    { model with
-      pgn = (id, Failed)::List.remove_assoc id model.pgn
-    }, Cmd.none
-  | Pgn_data (id, Result.Ok data) ->
-    { model with
-      pgn = (id, Received data)::List.remove_assoc id model.pgn
-    }, Cmd.msg (Validate_pgn data)
-  | Close_tab id ->
-    let pgn = List.remove_assoc id model.pgn in
-    let route = begin match pgn with
-      | (id, _)::_ -> Pgn id
-      | [] -> Tournament
-    end in
-    {model with pgn; route}, Cmd.none
-  | Validate_pgn pgn ->
-    try let position, ply, moves = Pgn.game_of_string pgn in
-      {model with game = {position; ply; moves}}, Cmd.none
-    with _e -> Js.log _e ; model, Cmd.none
+    route_of_location location |> update_route model
+  | _ -> model, Cmd.none
 
 
 
@@ -147,6 +193,7 @@ let header_nav_view =
 
 let tournament_view tournament =
   match tournament with
+  | Idle -> text ""
   | Loading -> text "Loading tournament..."
   | Received tournament' ->
     List.map
@@ -157,47 +204,61 @@ let tournament_view tournament =
     |> table []
   | Failed -> text "Tournament could not be loaded."
 
-let pgn_view id model =
-  try match List.assoc id model.pgn with
-    | Loading -> text "Loading PGN game..."
-    | Received _ -> Game.view model.game |> map game_msg
-    | Failed -> text "Game could not be loaded."             
-  with Not_found -> text ""
+
 
 let game_nav_view model =
-  let pgn_nav_item id =
-    li [ if model.route = Pgn id then class' "current" else noProp ]
-      [ if model.route = Pgn id
-        then text id
-        else a [href (Printf.sprintf "#/pgn/%s" id)] [text id]
-      ; span [] [text " "]
-      ; span [ style "cursor" "pointer"
-             ; onClick (Close_tab id)
-             ] [text "(x)"]
-      ] in
-
   let game_nav_item current link label =
     li [ if current then class' "current" else noProp ]
       [ if current then text label else a [href link] [text label] ] in
 
+  let g id _game k =
+    let item = game_nav_item 
+        (model.route = Local id)
+        (Printf.sprintf "#/local/%d" id)
+        (Printf.sprintf "Game %d" id)
+    in fun tl -> k (item::tl) in
+
+  (* http://blog.wakatta.jp/blog/2011/11/11/haskell-foldr-as-foldl/ *)
+
+(*
+  let f hd tl = game_nav_item 
+      (model.route = Local hd)
+      (Printf.sprintf "#/local/%d" hd)
+      (Printf.sprintf "Game %d" hd)::tl in
+
+  let g id _game k acc =
+    k (f id acc) in
+*)
+
   nav [class' "top tabbed"]
     [ ul []
-        ([ game_nav_item (model.route = Game) "#/game" "Game"
-         ; game_nav_item (model.route = Tournament) "#/tournament" "Tournament"
-         ] @ (List.rev_map (fun (id, _) -> pgn_nav_item id) model.pgn))
+        (game_nav_item (model.route = Scratch) "#/" "*scratch*"
+         ::(IntMap.fold g model.local_games (fun x -> x)) [])
     ]
 
+let scratch_view =
+  div []
+    [ nav []
+        [ button [onClick Save_scratch] [text "save this game"]
+        ; button [onClick Load_tournament] [text "load a game from Lichess"]
+        ]
+    ; p []
+        [ text "This is a scratch buffer.  Moves you make will not be saved in the browser's local storage." ]
+    ]
+
+
 let view model =
-  let game_status = Chess.game_status model.game.position in
+  let game = model |. model.game in
+  let game_view = Game.view game |> map game_msg in
   let interactable =
-    match game_status with
-    | Play move_list -> Board.Interactable (model.game.position.turn, move_list)
-    | _ -> Board.Not_interactable
-  in
+    match Chess.game_status game.position with
+    | Play move_list ->
+      Board.Interactable (game.position.turn, move_list)
+    | _ -> Board.Not_interactable in
   main []
     [ section [id "board"]
         [ header_nav_view
-        ; Board.view interactable model.game.position.ar model.board
+        ; Board.view interactable game.position.ar model.board
           |> map board_msg
           ;
           List.map (map board_msg) Board.buttons_view @
@@ -208,11 +269,11 @@ let view model =
     ; section [id "game"]
         [ game_nav_view model
         ; section [class' "scroll"]
-            [ match model.route with
-              | Game -> Game.view model.game |> map game_msg
-              | Tournament -> tournament_view model.tournament
-              | Pgn id -> pgn_view id model
-            ]
+            begin match model.route with
+              | Scratch -> scratch_view::[game_view]
+              | Tournament -> [tournament_view model.tournament]
+              | _ -> [game_view]
+            end
         ]
     ]
 
