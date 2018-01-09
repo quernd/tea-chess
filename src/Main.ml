75751aced5dd3abc264adb02791bd909a35fb446
Daniel Quernheim <quernd@users.noreply.github.com>
2018-01-09 19:55:06 +0000

    model is now O'Chess position and orientation, let's add some more action

--- src/Main.ml
+++ src/Main.ml
@@ -1,39 +1,46 @@
 open Tea
 
 type model =
-  { moves : int
-  ; turn : Ochess.color
+  { position : Ochess.position
+  ; orientation : Ochess.color
   }
 
 type msg =
-  | Move
+  | Flip_board
+  | Random_button
+  | Random_move of Ochess.move
 
 
 let init () =
-  { moves = 1
-  ; turn = White
+  { position = Ochess.init_position
+  ; orientation = White
   }, Cmd.none
 
 
 let update model = function
-  | Move ->
-    let turn = Ochess.opposite_color model.turn in
-    let moves = model.moves + 1 in
-    { turn; moves }, Cmd.none
+  | Flip_board ->
+    { model with
+      orientation = Ochess.opposite_color model.orientation },
+    Cmd.none
+  | Random_button -> model, Cmd.none
+  | Random_move _ -> model, Cmd.none
 
 
 let view model =
   let open Html in
   div []
     [ p [] [ Printf.sprintf "Move %d.  It is %s's move."
-               model.moves
-               (match model.turn with | Black -> "Black"
-                                      | White -> "White")
+               model.position.number
+               (match model.position.turn with | Black -> "Black"
+                                               | White -> "White")
              |> text
            ]
     ; p [] [ button
-               [ onClick Move ]
-               [ text "Make a move!" ]
+               [ onClick Flip_board ]
+               [ text "Flip board" ]
+           ; button
+               [ onClick Random_button ]
+               [ text "Make a random move!" ]
            ]
     ]
 
