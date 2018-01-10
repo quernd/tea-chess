4be2bcd0d6d26e8db2017f99f051642aa6f31484
Daniel Quernheim <quernd@users.noreply.github.com>
2018-01-10 12:49:59 +0000

    added src/Game.ml and prepared move logging

---
+++ src/Game.ml
@@ -0,0 +1,62 @@
+open Tea
+
+type san = string
+
+type move =
+  { move : Chess.move
+  ; san : san
+  }
+
+type model =
+  { position : Chess.position
+  ; moves : move list
+  }
+
+type msg =
+  | Move of Chess.move
+  | Take_back
+[@@bs.deriving {accessors}]    
+
+let init =
+  { position = Chess.init_position
+  ; moves = []
+  }
+
+
+let simple_move move san =
+  { move = move
+  ; san = san
+  }
+
+
+let update model = function
+  | Move move ->
+    begin try
+        let position = Chess.make_move model.position move 0 in
+        { model with position
+                   ; moves = simple_move move "splendid move" :: model.moves
+        }, Cmd.none
+      with Chess.Illegal_move -> model, Cmd.none
+    end
+  | Take_back ->
+    begin match model.position.prev, model.moves with
+      | Some position, _hd::moves ->
+        { model with position; moves }, Cmd.none
+      | _ -> model, Cmd.none
+    end
+
+
+let view model =
+  let open Html in
+  let move_view move =
+    li [] [ text move.san ] in
+
+  div []
+    [ p [] [ Printf.sprintf "Move %d.  It is %s's move."
+               model.position.number
+               (match model.position.turn with | Black -> "Black"
+                                               | White -> "White")
+             |> text
+           ]
+    ; List.rev_map move_view model.moves |> ul []
+    ]
--- src/Main.ml
+++ src/Main.ml
@@ -2,33 +2,36 @@ open Tea
 open App
 
 type model =
-  { position : Chess.position
+  { game : Game.model
   ; board : Board.model
   }
 
 type msg =
   | Board_msg of Board.msg
+  | Game_msg of Game.msg
   | Random_button
   | Random_move of Chess.move
 [@@bs.deriving {accessors}]
 
 
 let init () =
-  { position = Chess.init_position
+  { game = Game.init
   ; board = Board.init
   }, Cmd.none
 
 
 let update model = function
-  | Board_msg (Move move) ->
-    let position = Chess.make_move model.position move 0 in
-    { model with position }, Cmd.none
+  | Board_msg (Move move) | Random_move move ->
+    model, Game_msg (Move move) |> Cmd.msg
   | Board_msg msg ->
     let board, cmd = Board.update model.board msg in
     { model with board }, Cmd.map board_msg cmd
+  | Game_msg msg ->
+    let game, cmd = Game.update model.game msg in
+    { model with game }, Cmd.map game_msg cmd
   | Random_button ->
     model,
-    begin match Chess.game_status model.position with
+    begin match Chess.game_status model.game.position with
       | Play move_list ->
         move_list
         |> List.length
@@ -38,31 +41,26 @@ let update model = function
              List.nth move_list random_number |> random_move)
       | _ -> Cmd.none
     end
-  | Random_move move ->
-    let position = Chess.make_move model.position move 0 in
-    { model with position }, Cmd.none
 
 
 let view model =
   let open Html in
   let interactable =
-    match Chess.game_status model.position with
+    match Chess.game_status model.game.position with
     | Play move_list ->
-      Board.Interactable (model.position.turn, move_list)
+      Board.Interactable (model.game.position.turn, move_list)
     | _ -> Board.Not_interactable in
   div []
-    [ Board.view interactable model.position.ar model.board |> map board_msg
-    ; p [] [ Printf.sprintf "Move %d.  It is %s's move."
-               model.position.number
-               (match model.position.turn with | Black -> "Black"
-                                               | White -> "White")
-             |> text
-           ]
+    [ Board.view interactable model.game.position.ar model.board |> map board_msg
     ; p [] [ map board_msg Board.flip_button_view
            ; button
                [ onClick Random_button ]
                [ text "Make a random move!" ]
+           ; button
+               [ onClick (Game_msg Take_back) ]
+               [ text "Take back" ]
            ]
+    ; Game.view model.game
     ]
 
 
