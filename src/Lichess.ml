332e847416bec0596087d6d2c756cb2078cb1206
Daniel Quernheim <quernd@users.noreply.github.com>
2020-12-25 20:32:23 +0100

    changes to Lichess tournament API

--- src/Lichess.ml
+++ src/Lichess.ml
@@ -17,38 +17,81 @@ type model = (string * string list) list transfer
 
 let init = Idle
 
-let proxy = "http://localhost:3000/fetch/"
+let make_request url headers action =
+  let request = 
+    { Http.method' = "GET"
+    ; headers = headers
+    ; url = url
+    ; body = Web.XMLHttpRequest.EmptyBody
+    ; expect = Http.expectString
+    ; timeout = None
+    ; withCredentials = false
+    }
+  in
+  Http.request request
+  |> Http.send action
 
+let rec filter games =
+  let open Tea.Result in
+  match games with
+    [] -> []
+  | (Ok game)::tl -> game::filter tl
+  | (Error _)::tl -> filter tl
+
+let split_on_char sep str =
+  let rec indices acc i =
+    try
+      let i = succ(String.index_from str i sep) in
+      indices (i::acc) i
+    with Not_found ->
+      (String.length str + 1) :: acc
+  in
+  let is = indices [0] 0 in
+  let rec aux acc = function
+    | last::start::tl ->
+      let w = String.sub str start (last-start-1) in
+      aux (w::acc) (start::tl)
+    | _ -> acc
+  in
+  aux [] is
 
 let get_game msg game_id =
-  Printf.sprintf
-    "%shttps://lichess.org/game/export/%s.pgn" proxy game_id
-  |> Http.getString
-  |> Http.send msg
+  let url = (Printf.sprintf
+               "https://lichess.org/game/export/%s.pgn" game_id) in
+  make_request url [] msg
 
 let update model = function
   | Load_tournament ->
     begin match model with
       | Loading | Received _ -> model, Cmd.none
       | Idle | Failed ->
-        let url = "https://lichess.org/api/tournament/GToVqkC9" in
+        let url = "https://lichess.org/api/tournament/a3lZhGdA/games?moves=false" in
         model,
-        Http.getString url |> Http.send tournament_data
+        make_request
+          url
+          [Http.Header ("Accept", "application/x-ndjson")]
+          tournament_data
     end
   | Tournament_data (Error _e) ->
+    Js.log (Http.string_of_error(_e));
     Failed, Cmd.none
-  | Tournament_data (Ok data) -> 
+  | Tournament_data (Ok data) ->
     let open Json.Decoder in
-    let players_decoder = list string in
-    let pairing_decoder = map2 (fun x y -> x, y)
-        (field "id" string)
-        (field "u" players_decoder) in
-    let list_decoder = list pairing_decoder in
-    let pairings_decoder = field "pairings" list_decoder in
-    begin match decodeString pairings_decoder data with
-      | Ok tournament -> Received tournament
-      | Error _ -> Failed
-    end, Cmd.none
+    let id_decoder = field "id" string in
+    let player_decoder = (field "user" (field "name" string)) in
+    let players_decoder = field "players"
+        (map2 (fun x y -> [x; y])
+           (field "white" player_decoder)
+           (field "black" player_decoder)) in
+
+    let game_decoder = map2 (fun x y -> x, y)
+        id_decoder
+        players_decoder in
+    let games = split_on_char '\n' data in
+
+    let games_decoded = List.map (decodeString game_decoder) games |> filter in
+    Received games_decoded, Cmd.none
+
   | Load_game game_id ->
     model, get_game (game_data game_id) game_id
   | Game_data _ -> model, Cmd.none
